5bcf837f7fc97d24680b4e0fd8ea5f8dc1316c26
Siim Koppel <siimk999@gmail.com>
2023-09-20 10:04:45 +0300

    Include bootstrap library

--- chapter7.php
+++ chapter7.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Bootstrap library css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
 <h1>Chapter 7</h1>
 <h2>Greeting function</h2>
 <?php
@@ -7,3 +14,9 @@ function greeting()
 }
 echo greeting();
 ?>
+
+
+<!-- Bootstrap library javascript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
