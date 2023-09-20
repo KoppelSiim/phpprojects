49f59cb80a669af1c2ad6568e786dcfa1aa2b04f
Siim Koppel <siimk999@gmail.com>
2023-09-20 12:33:41 +0300

    Minor changes

--- chapter7.php
+++ chapter7.php
@@ -2,6 +2,7 @@
 // Start or resume the session
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,7 +21,10 @@ function greeting()
 }
 echo greeting();
 ?>
+
 <h4>Bootstrap form </h4>
+
+<!--Bootstrap Form row and column -->
 <div class="row">
     <div class="col-3">
 <?php
@@ -36,7 +40,7 @@ function generate_form()
             <label for="email" class="form-label">E-mail</label>
             <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary mb-3">Submit</button>
         </form>
         ');
 
@@ -105,12 +109,16 @@ function generate_form()
         }
     }
 }
+
 generate_form();
 ?>
+
 </div>
     </div>
+<!--Bootstrap Form row and column end -->
 
 </container>
+
 <!-- Bootstrap library javascript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
