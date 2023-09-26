868f8ef6d6fbc00edd4195aa12990bc487575f8c
Siim Koppel <siimk999@gmail.com>
2023-09-26 10:03:25 +0300

    Teated lehelink, kassid implementation ja link

--- juku/server.php
+++ juku/server.php
@@ -1,9 +1,6 @@
 <?php
     require ($_SERVER["DOCUMENT_ROOT"]."/../config.php");
     global $connect;
-    $req = $connect -> prepare("SELECT id, pealkiri, sisu FROM lehed");
-    $req -> bind_result($id, $pealkiri, $sisu);
-    $req -> execute();
 ?>
 
 <!DOCTYPE html>
@@ -15,42 +12,34 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Teadete loetelu</h1>
-    
+    <ol>
+        <li><a href="?page=teated">Teated</a></li>
+        <li><a href="?page=kassid">Kassid</a></li>
+    </ol>
     <?php
+        if (isset($_GET['page']) && $_GET['page'] == 'teated') {
+            echo"<h1>Teadete loetelu</h1>";
+            $req = $connect -> prepare("SELECT id, pealkiri, sisu FROM lehed");
+            $req -> bind_result($id, $pealkiri, $sisu);
+            $req -> execute();
+        
+            while($req ->fetch()){
+                echo "<h2>".htmlspecialchars($pealkiri)."</h2>";
+                echo "<h4>".htmlspecialchars($sisu)."</h4>";
+            }
+        }
+    ?>
 
-       while($req ->fetch()){
-            echo "<h2>".htmlspecialchars($pealkiri)."</h2>";
-            echo "<h4>".htmlspecialchars($sisu)."</h4>";
-        }
-    ?>
-    <h1>Ülesandeid</h1>
-    <h4>Väljasta kassid</h4>
     <?php
-        require ($_SERVER["DOCUMENT_ROOT"]."/../config.php");
-        $req = $connect -> prepare("SELECT id, nimi, toon FROM kassid"); 
-        $req -> bind_result($id, $nimi, $toon);
-        $req -> execute();
-        while($req ->fetch()){
-            echo "<h2>".htmlspecialchars($nimi)."</h2>";
-            echo "<h4>".htmlspecialchars($toon)."</h4>";
-        }
-    ?>
-     <!-- Kasside lisamine
-        $insert = $connect -> prepare("INSERT INTO kassid(nimi,toon) VAlUES('Musike','Helekuldne'), ('Kullakallike','Tumesinine');");
-        if ($insert->execute()) {
-            echo "New record created successfully";
-        } else {
-            echo "Unable to create record";
-        } -->
-    <h4>Kuva kassid toonide järjekorras</h4>
-    <?php
-        $cats = $connect -> prepare("SELECT id, nimi, toon FROM kassid ORDER BY toon ASC;");
+    if (isset($_GET['page']) && $_GET['page'] == 'kassid') {
+        echo"<h1>Kassid</h1>";
+        $cats = $connect -> prepare("SELECT id, nimi, toon FROM kassid");
         $cats -> bind_result($id, $nimi, $toon);
         $cats -> execute();
         while($cats->fetch()){
-            echo "<h5>".htmlspecialchars($toon)."</h5>";
+            echo "<h5 style ='color:$toon;'>".htmlspecialchars($toon)."</h5>";
         }
+    }
     ?>
 
 </body>
