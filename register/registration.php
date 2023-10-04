47f315c5c8caf5ab92a4c68bb84fa752a810a1af
Siim Koppel <siimk999@gmail.com>
2023-10-04 11:05:38 +0300

    Small style changes

--- register/registration.php
+++ register/registration.php
@@ -1,18 +1,20 @@
 
 <div class="container">
     <form class="submitForm">
-        <div class = form-item">
+        <div class="form-item">
             <label for="firstName" class="form-label">Eesnimi</label>
             <input type="text" class="form-control" id="firstName" name="firstName" placeholder="Sisesta eesnimi ...">
         </div>
-        <div class = "form-item">
+        <div class="form-item">
             <label for="lastName" class="form-label">Perekonnanimi</label>
             <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Sisesta perekonnanimi ...">
         </div> 
-        <div class = "form-item">
+        <div class="form-item">
             <label for="email" class="form-label">Emaili aadress</label>
             <input type="email" class="form-control" id="email" name="email" placeholder="Sisesta meiliaadress ...">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-item">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
     </form>
 </div>
