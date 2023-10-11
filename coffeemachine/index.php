24374802cf5ba68bf0edd6dac98364cff458f569
Siim Koppel <siimk999@gmail.com>
2023-10-11 22:00:27 +0300

    Add user page, user drink

--- coffeemachine/index.php
+++ coffeemachine/index.php
@@ -2,6 +2,17 @@
 
     require ($_SERVER["DOCUMENT_ROOT"]."/../config.php");
     global $connect;
+    if(isSet($_REQUEST["update-drink"])){
+        $sqlUpdateCups=$connect->prepare("UPDATE coffee_machine SET cups = cups - 1 WHERE id = ?");
+        $sqlUpdateCups->bind_param('i', $_REQUEST['update-drink']);
+        if ($sqlUpdateCups->execute()) {
+            header("Location: $_SERVER[PHP_SELF]");
+        }else {
+            echo "Error: " . $sqlUpdateCups->error;
+        }
+        $connect->close();
+        exit();
+    }
     require("header.php");
     
     if(isset($_GET["page"])) {
---
+++ coffeemachine/user.php
@@ -0,0 +1,19 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/../config.php");
+$sqlDrinkQuery = $connect->prepare("SELECT id, name, package, cups FROM coffee_machine");
+$sqlDrinkQuery->bind_result($id,$name,$package,$cups);
+$sqlDrinkQuery->execute();
+?>
+<h1>Joogiautomaat</h1>
+<div class="formSubmit" action="?">
+<?php
+    while($sqlDrinkQuery->fetch()){
+        echo "<div id='form-item'>";
+            echo htmlspecialchars($name). "</br>";
+            echo htmlspecialchars($cups). "</br>";
+            echo "<a href='?update-drink=$id'>Joo</a>";
+        echo "</div>";
+    }
+    $sqlDrinkQuery->close();
+?>
+</div>
